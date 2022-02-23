d7280e8d7ef2a381f6cc5b5f036337f3d141827a
Will <willzhaoshihao@gmail.com>
2022-02-23 11:21:05 +0800

    Add polkdot account balance sync service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { StatusMonitorModule } from 'nestjs-status-monitor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PolkadotBalanceAnalysisModule } from './polkadot-balance/polkadot-balance-analysis.module';
+import { ScheduleModule } from '@nestjs/schedule';
 @Module({
-  imports: [StatusMonitorModule.forRoot(), PolkadotBalanceAnalysisModule],
+  imports: [
+    StatusMonitorModule.forRoot(), 
+    PolkadotBalanceAnalysisModule,
+    ScheduleModule.forRoot(),],
   controllers: [AppController],
   providers: [AppService],
 })
